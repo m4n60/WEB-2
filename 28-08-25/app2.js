8c4ed504a96b479e1bb69cc592b1bba5b4fe7675
m4n60 <armandlopez17@gmail.com>
2025-08-29 09:00:00 -0600

    Modificación

--- 28-08-25/app2.js
+++ 28-08-25/app2.js
@@ -14,8 +14,8 @@ let and3 = 5==5 && 4< 5;
 console.log([and, and1, and2, and3].join("\n"));
 
 
-let or = 20 == 10 && 5 < 3;
-let or1 = 20 == 20 && 9 < 7;
-let or2 = 8 < 5 && 5 == 5;
-let or3 = 5==5 && 4< 5;
+let or = 20 == 10 || 5 < 3;
+let or1 = 20 == 20 || 9 < 7;
+let or2 = 8 < 5 || 5 == 5;
+let or3 = 5==5 || 4< 5;
 console.log([or, or1, or2, or3].join("\n"));
