f23abe86610f9e681f0da5ea31da9b78701d9c48
m4n60 <armandlopez17@gmail.com>
2025-09-24 22:07:19 -0600

    tarea18092025

--- 18-09-2025/app4.js
+++ 18-09-2025/app4.js
@@ -1,31 +1,40 @@
-var productos =[
-    {nombre: 'camisa',precio : 100},
-    {nombre: 'pantalon', precio : 200},
-    {nombre: 'zapatos', precio: 500},
-    {nombre: 'gorra', precio:150}
-
+var productos = [
+    {nombre: 'camisa', precio: 300},
+    {nombre: 'pantalon', precio: 500},
+    {nombre: 'zapatos', precio: 400},
+    {nombre: 'sombrero', precio: 200}
 ];
 
-var carrito= [];
+var carrito = [];
+var total = 0;
+var opcion = 0; 
 
-function agregarProducto(){
-let car = prompt(
-    ' 1. Camisa : $100' +
-    '\n 2. Pantalon : $200' + 
-    '\n 3. Zapatos GUCCI: $500' + 
-    '\n 4. Gorra : $150'+ 
-    '\n ¿Que desea comprar?'
+while (opcion !== productos.length + 1) {
+    // Mostrar lista de productos
+    var menu = "Selecciona un producto para agregar al carrito:\n";
+    for (var i = 0; i < productos.length; i++) {
+        menu += (i + 1) + ". " + productos[i].nombre + " - $" + productos[i].precio + "\n";
+    }
+    menu += (productos.length + 1) + ". Terminar compra";
 
-);
+    // Elige un producto
+    opcion = parseInt(prompt(menu));
 
-if (car) {
-    preducto.push(car);
-
-    alert('P')
-} else {
-    
+    if (opcion >= 1 && opcion <= productos.length) {
+        var producto = productos[opcion - 1];
+        carrito.push(producto);
+        total += producto.precio;
+        alert(producto.nombre + " agregado al carrito. Total actual: $" + total);
+    } else if (opcion !== productos.length + 1) {
+        alert("Opción inválida, intenta de nuevo.");
+    }
 }
+
+// Mostrar lo que se compro 
+var resumen = "Tu carrito contiene:\n";
+for (var j = 0; j < carrito.length; j++) {
+    resumen += "- " + carrito[j].nombre + " $" + carrito[j].precio + "\n";
 }
+resumen += "Total a pagar: $" + total;
 
-//Ponerlo chingon 
-
+alert(resumen);
